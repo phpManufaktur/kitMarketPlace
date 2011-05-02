f9500314db028adc2d13f845eb3e39978d765e7a
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-05-02 03:56:50 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitMarketPlace/trunk@2 ca2d9673-a5a9-ba4d-935a-6d6421dafb50

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * kitMarketPlace
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+$module_directory     = 'kit_market';
+$module_name          = 'kitMarketPlace';
+$module_function      = 'tool';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8'; 
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License';
+$module_description   = 'KIT Market Place Extension'; 
+$module_home          = 'http://phpmanufaktur.de/kit_market';
+$module_guid          = 'FDD80EFD-80FA-4A7E-9D64-AE37E3BEB6F7';
+
+?>
